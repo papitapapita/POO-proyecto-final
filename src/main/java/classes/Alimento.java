974a160547e7ee0edb5f98914f365e10ca256b74
Jonathan <many.d.r.o.2001@hotmail.com>
2024-11-14 16:10:36 -0500

    Create alimentos class with methods and attributes

--- src/main/java/classes/Alimento.java
+++ src/main/java/classes/Alimento.java
@@ -1,5 +1,83 @@
 package classes;
 
-public class Alimento {
-    
+public abstract class Alimento {
+    String nombre;
+    String grupo;
+    double calorias;
+    double pesoPorPorcion;
+    String unidadMedida;
+    String origen;
+
+    public Alimento(String nombre, String grupo, double calorias, double pesoPorPorcion, String unidadMedida, String origen) {
+        this.nombre = nombre;
+        this.grupo = grupo;
+        this.calorias = calorias;
+        this.pesoPorPorcion = pesoPorPorcion;
+        this.unidadMedida = unidadMedida;
+        this.origen = origen;
+    }
+
+    public abstract String obtenerInformacionNutricional();
+
+    double calcularCalorias(double cantidad) {
+        return (cantidad / pesoPorPorcion) * calorias;
+    }
+
+    boolean esSaludable() {
+        return calorias <= 200;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public double getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(double calorias) {
+        this.calorias = calorias;
+    }
+
+    public double getPesoPorPorcion() {
+        return pesoPorPorcion;
+    }
+
+    public void setPesoPorPorcion(double pesoPorPorcion) {
+        this.pesoPorPorcion = pesoPorPorcion;
+    }
+
+    public String getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Grupo: " + grupo + ", Calorías: " + calorias +
+                ", Peso por porción: " + pesoPorPorcion + " " + unidadMedida + ", Origen: " + origen;
+    }
 }
