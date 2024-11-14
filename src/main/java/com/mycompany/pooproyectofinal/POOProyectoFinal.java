fc9977e9afb32e3991225c3ef7fddf7d526f0ea4
Jonathan <many.d.r.o.2001@hotmail.com>
2024-11-14 15:17:39 -0500

    Creation of the base classes

---
+++ src/main/java/com/mycompany/pooproyectofinal/POOProyectoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooproyectofinal;
+
+/**
+ *
+ * @author manyd
+ */
+public class POOProyectoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
